4d6b2666e97fabb0c32dcfdc7a60e0ae23da294e
ideal402 <ideal402@naver.com>
2026-02-22 03:16:06 +0900

    로고 추가 / 버튼 css 변경

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,17 +7,17 @@ export const Navbar: React.FC = () => {
 
   // 토글 버튼 배열 (id, 레이블, 아이콘 매핑)
   const toggleButtons = [
-    { id: 'population', label: '인구밀집', icon: '👥' },
-    { id: 'weather', label: '기상상태', icon: '⛅' },
-    { id: 'air', label: '대기질', icon: '🍃' },
-    { id: 'bus', label: '버스유동인구', icon: '🚌' },
+    { id: 'population', label: '인구밀집', icon: '/Users.svg' },
+    { id: 'weather', label: '기상상태', icon: '/Sun.svg' },
+    { id: 'air', label: '대기질', icon: '/Leaf.svg' },
+    { id: 'bus', label: '버스유동인구', icon: '/Car_Auto.svg' },
   ];
 
   return (
     <nav className="navbar-container">
       {/* 좌측 영역 */}
       <div className="navbar-left">
-        <img src="../../public/logo.png" alt="Logo" className="logo-img" style={{height:"56px", width: "56px"}}/>
+        <img src="/logo.png" alt="Logo" className="logo-img" style={{height:"56px", width: "56px"}}/>
         <button 
           className="logo-btn" 
           onClick={() => console.log('지도 홈으로 이동')}
@@ -37,7 +37,7 @@ export const Navbar: React.FC = () => {
               onClick={() => setActiveLayer(btn.id)}
               title={btn.label}
             >
-              <span className="hex-icon">{btn.icon}</span>
+              <img src={btn.icon} className="hex-icon" />
             </button>
           ))}
         </div>
